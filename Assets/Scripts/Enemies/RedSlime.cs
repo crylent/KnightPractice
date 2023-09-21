53dd52a893d47c9c5d8876214e9eeae88d6dac6d
Somnie Maxim Belkovsky <inkerimaa1@gmail.com>
2023-09-21 17:30:03 +0300

    Red slimes

---
+++ Assets/Scripts/Enemies/RedSlime.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class RedSlime : Slime
+    {
+        [SerializeField] private GameObject fireEffect;
+        [SerializeField] private float fireCooldown = 1f;
+        [SerializeField] private float fireLifetime = 5f;
+    
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+            StartCoroutine(SetGroundOnFire());
+        }
+
+        private IEnumerator SetGroundOnFire()
+        {
+            while (isActiveAndEnabled)
+            {
+                var fire = Instantiate(fireEffect, transform.position, fireEffect.transform.rotation);
+                StartCoroutine(PutOutFire(fire.GetComponentInChildren<ParticleSystem>()));
+                yield return new WaitForSeconds(fireCooldown);
+            }
+        }
+
+        private IEnumerator PutOutFire(ParticleSystem fireSystem)
+        {
+            yield return new WaitForSeconds(fireLifetime);
+            fireSystem.Stop();
+            yield return new WaitWhile(fireSystem.IsAlive);
+            Destroy(fireSystem.transform.parent);
+        }
+    }
+}
