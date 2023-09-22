dcb08cf953c8c277de24e7a4caba7f770bd773d3
Somnie Maxim Belkovsky <inkerimaa1@gmail.com>
2023-09-22 11:41:02 +0300

    Dodge mechanics

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Enemies;
 using UnityEngine;
@@ -9,10 +10,15 @@ namespace Player
 {
     public class PlayerController : LiveEntity
     {
-        [SerializeField] private float speed = 25;
+        [SerializeField] private float speed = 50f;
         private Vector3 _deltaMove;
         private bool _watchingRight;
-    
+        
+        [SerializeField] private float dodgeSpeed = 200f;
+        [SerializeField] private float dodgeTime = 0.15f;
+        [SerializeField] private ParticleSystem dodgeEffect;
+        private bool _isDodging;
+
         [SerializeField] private Collider attackHitbox;
         private Collider _attackHitbox;
         private HashSet<Enemy> _enemiesBeingAttacked; // enemies inside the attack hitbox
@@ -30,6 +36,7 @@ namespace Player
 
         private void FixedUpdate()
         {
+            if (_isDodging) return;
             var movement = !IsAttacking ? _deltaMove : Vector3.zero; // can't move when attacking
             Rigidbody.velocity = speed * movement;
         }
@@ -53,8 +60,7 @@ namespace Player
 
         public void OnAttack(InputAction.CallbackContext context)
         {
-            if (!context.performed) return;
-            if (IsAttacking) return;
+            if (!context.performed || IsAttacking) return;
             
             IsAttacking = true;
             Animator.SetTrigger(AttackTrigger);
@@ -67,6 +73,32 @@ namespace Player
                 hitboxTransform.rotation = Quaternion.Euler(hitboxRot);
             }
             _enemiesBeingAttacked = _attackHitbox.GetComponent<ColliderController>().Enemies;
+        }
+
+        public void OnDodge(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+            if (!_isDodging) StartCoroutine(Dodge());
+        }
+
+        private IEnumerator Dodge()
+        {
+            _isDodging = true;
+            
+            yield return new WaitWhile(() => IsAttacking); // can't dodge while attacking, but will do it after finishing
+            var effectTransform = dodgeEffect.transform;
+            var system = Instantiate(
+                dodgeEffect, 
+                transform.position + effectTransform.position, 
+                effectTransform.rotation
+                );
+            Rigidbody.velocity = _deltaMove * dodgeSpeed;
+            
+            yield return new WaitForSeconds(dodgeTime);
+            _isDodging = false;
+            
+            yield return new WaitWhile(system.IsAlive);
+            Destroy(system.gameObject);
         }
 
         public override void MakeDamage()
