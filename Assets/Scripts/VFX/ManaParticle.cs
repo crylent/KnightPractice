1c17843e2e1b0180ade21da22882b6cc780beb2c
Somnie Maxim Belkovsky <inkerimaa1@gmail.com>
2023-12-30 14:28:50 +0300

    Mana VFX minor fix

--- Assets/Scripts/VFX/ManaParticle.cs
+++ Assets/Scripts/VFX/ManaParticle.cs
@@ -1,20 +1,14 @@
 using Player;
 using UnityEngine;
-using Utility;
 
 namespace VFX
 {
     public class ManaParticle : MonoBehaviour
     {
         public float manaValue = 1f;
-        [SerializeField] private float speed = 10f;
+        [SerializeField] private float speed = 0.5f;
         
         private static Vector3 PlayerPosition => PlayerComponents.Transform.position;
-
-        private void Start()
-        {
-            Effects.DestroyOnStopped(GetComponentInChildren<ParticleSystem>());
-        }
 
         private void Update()
         {
